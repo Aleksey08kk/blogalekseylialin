08163e71f009248c6c67f00329850781b8d819bf
Aleksey08kk <93997625+Aleksey08kk@users.noreply.github.com>
2023-02-17 10:22:41 +0400

    Теги
    Сделал связи многие к многим. Теги к статьям и статьи к тегам. Сохранение/удаление/перезапись тегов в БД

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -6,6 +6,7 @@ use app\models\Article;
 use app\models\ArticleSearch;
 use app\models\Category;
 use app\models\ImageUpLoad;
+use app\models\Tag;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
@@ -164,10 +165,25 @@ class ArticleController extends Controller
             if($article->saveCategory(($category))){
                 return $this->redirect(['view', 'id' => $article->id]);
             }
+        }
+        return $this->render('category', ['article' => $article, 'selectedCategory' => $selectedCategory, 'categories' => $categories]);
+    }
 
+    public function actionSetTags($id){
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags(); //
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+
+        if(Yii::$app->request->isPost){
+            $tags = Yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id'=>$article->id]);
         }
 
-        return $this->render('category', ['article' => $article, 'selectedCategory' => $selectedCategory, 'categories' => $categories]);
+        return $this->render('tags', [
+            'selectedTags'=>$selectedTags,
+            'tags'=>$tags
+        ]);
     }
 
 }
